d9a4ab0e8f8e12ed442d077dacd2b2c77f7d5cad
Nikinox <gianmy201011@gmail.com>
2026-06-03 19:42:14 +0200

    Create main.py

---
+++ DumbMathGame/main.py
@@ -0,0 +1,16 @@
+from random import randint
+
+while (max := int(input("insert a maximum number\n"))) <= 1:
+        print("error, insert again a > 1")
+        max = int(input())
+
+while True:
+
+    value = randint(1, max)
+    value1 = randint(1, value)
+
+    print(value, " + ", value1, " = ")
+
+    result = int(input())
+
+    print("correct" if value + value1 == result else "wrong")
