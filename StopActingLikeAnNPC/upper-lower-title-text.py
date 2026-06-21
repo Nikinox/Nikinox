37fe9aed14b8f6909f80d3e0486d6fd7c2072750
Nikinox <gianmy201011@gmail.com>
2026-06-21 21:20:10 +0200

    adding the code
    Add functionality to convert input text to upper, lower, or title case based on user choice.

--- StopActingLikeAnNPC/upper-lower-title-text.py
+++ StopActingLikeAnNPC/upper-lower-title-text.py
@@ -1,1 +1,11 @@
-
+while True:
+  a = input('insert text')
+  while (choice = input('choose if upper (U), lower (L) or title (T)').lower()) not in ["l", "u", "t"]:
+    print('error, reinsert the choice')
+  match choice:
+    case "l":
+      print(a.lower())
+    case "t":
+      print(a.title())
+    case "u":
+      print(a.upper())
