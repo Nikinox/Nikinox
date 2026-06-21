3d9ee3e2ff1f2a17a0ca4ccccde4ecb1d2a50acd
Nikinox <gianmy201011@gmail.com>
2026-06-21 21:22:15 +0200

    improving and compacting the code

--- StopActingLikeAnNPC/upper-lower-title-text.py
+++ StopActingLikeAnNPC/upper-lower-title-text.py
@@ -1,11 +1,8 @@
 while True:
   a = input('insert text')
-  while (choice = input('choose if upper (U), lower (L) or title (T)').lower()) not in ["l", "u", "t"]:
+  while (choice := input('choose if upper (U), lower (L) or title (T)').lower()) not in ["l", "u", "t"]:
     print('error, reinsert the choice')
   match choice:
-    case "l":
-      print(a.lower())
-    case "t":
-      print(a.title())
-    case "u":
-      print(a.upper())
+    case "l": print(a.lower())
+    case "t": print(a.title())
+    case "u": print(a.upper())
