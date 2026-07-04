59c8eab7f7191232c307097d5fbfc2983192d685
Nikinox <gianmy201011@gmail.com>
2026-07-04 15:27:39 +0200

    improving the tool by adding error checking
    v2.0

--- StopActingLikeAnNPC/sorting_words.py
+++ StopActingLikeAnNPC/sorting_words.py
@@ -1,3 +1,7 @@
 while True:
-	list=sorted(input('insert dividing with spaces:\n').split())
-	print(' '.join(list))
+    while (choice := input("choose if sorted (S) or reverse sorted (R): ").lower()) not in ["s", "r"]:
+        print("error, insert again the choice")
+
+    words = input("insert dividing with spaces:\n").split()
+
+    print(" ".join(sorted(words)) if choice == "s" else " ".join(sorted(words, reverse=True)))
