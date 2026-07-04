89d682153d7daf8f72fbbaee935f1bb0a895166c
Nikinox <gianmy201011@gmail.com>
2026-07-04 15:38:43 +0200

    removing the saving in the variable
    v3.0

--- StopActingLikeAnNPC/upper-lower-title-text.py
+++ StopActingLikeAnNPC/upper-lower-title-text.py
@@ -1,8 +1,7 @@
 while True:
-  a = input('insert text')
   while (choice := input('choose if upper (U), lower (L) or title (T)').lower()) not in ["l", "u", "t"]:
     print('error, reinsert the choice')
   match choice:
-    case "l": print(a.lower())
-    case "t": print(a.title())
-    case "u": print(a.upper())
+    case "l": print(input("insert text:\n").lower())
+    case "t": print(input("insert text:\n").title())
+    case "u": print(input("insert text:\n").upper())
