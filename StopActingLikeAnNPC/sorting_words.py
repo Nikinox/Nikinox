e3da3df9d1a6d4fa81467c3dec4ce4405e3ac8b5
Nikinox <gianmy201011@gmail.com>
2026-07-13 23:58:13 +0200

    compacting the code

--- StopActingLikeAnNPC/sorting_words.py
+++ StopActingLikeAnNPC/sorting_words.py
@@ -2,6 +2,4 @@ while True:
     while (choice := input("choose if sorted (S) or reverse sorted (R): ").lower()) not in ["s", "r"]:
         print("error, insert again the choice")
 
-    words = input("insert dividing with spaces:\n").split()
-
-    print(" ".join(sorted(words)) if choice == "s" else " ".join(sorted(words, reverse=True)))
+    print(" ".join(sorted(input("insert with spaces:\n").split())) if choice == "s" else " ".join(sorted(input("insert with spaces:\n").split(), reverse=True)))
